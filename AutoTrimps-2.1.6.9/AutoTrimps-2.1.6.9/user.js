049df5e689b7ee2f17364d23d035465914b2b8f7
KeMa1717 <kevinmartin1717@hotmail.com>
2018-12-31 02:35:42 +0100

    Add files via upload

---
+++ AutoTrimps-2.1.6.9/AutoTrimps-2.1.6.9/user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         AutoTrimps-genBTC
+// @namespace    https://github.com/genbtc/AutoTrimps
+// @version      2.1.6.9-genbtc-3-23-2018
+// @updateURL    https://github.com/genbtc/AutoTrimps/user.js
+// @description  Automate all the trimps!
+// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @include        *trimps.github.io*
+// @include        *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.id = 'AutoTrimps-script';
+//This can be edited to point to your own Github Repository URL.
+script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+//script.setAttribute('crossorigin',"use-credentials");
+//script.setAttribute('crossorigin',"anonymous");
+document.head.appendChild(script);
+
+// This File is Deprecated, please use .user.js instead (with the leading dot) so TamperMonkey/Greasemonkey can detect it as a script automatically.
+// Other than that they are identical and no change is required on your part if everything is working already.
